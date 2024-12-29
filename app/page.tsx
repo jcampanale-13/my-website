d3db0c9680e6951483d5d10aed60c856b90dcabd
Jack Campanale <jcampanale54@gmail.com>
2024-12-29 13:51:16 -0500

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import NavBar from "./components/navbar/page";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar/>
+      <h1>HELLO</h1>
+    </div>
+  );
+}
