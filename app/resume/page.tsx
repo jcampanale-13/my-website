4dd702ebc8f5f12d0ee0d7d856a620a78a8a9818
Jack Campanale <jcampanale54@gmail.com>
2025-05-08 15:28:12 -0400

    update resume again

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,7 @@
 export default function Resume() {
     return (
         <div className="flex justify-center my-32">
-            <iframe src="https://drive.google.com/file/d/1--JwIfYOGeIJgZy3IBllnQ1lYWKNH2PH/preview" width="640" height="640" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1IdBFisN7wBqN1Zoy00f1ec51N-xcz8jg/preview" width="640" height="640" allow="autoplay"></iframe>
         </div>
     );
 }
